559ff849c8523816c8d356c9cd8d6924ea600b0c
Weldu Berhe <wel.berhe@gmail.com>
2014-09-20 15:07:07 -0500

    service implementation updated

---
+++ New Folder/WAMP_blog_engine/src/main/java/cs544/wamp_blog_engine/contoller/UserController.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cs544.wamp_blog_engine.contoller;
+
+/**
+ *
+ * @author Weldino
+ */
+public class UserController {
+    
+    //TODO
+    
+}
