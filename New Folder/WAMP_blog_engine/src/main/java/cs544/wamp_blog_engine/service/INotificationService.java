d5ba8a336bb790f664bf86fd0aa03e2f27ed640f
aalzamer <aalzamer@CS_LAB218-109.cs.mum.edu>
2014-09-20 15:06:18 -0500

    changed interface to implement needed functionality

--- New Folder/WAMP_blog_engine/src/main/java/cs544/wamp_blog_engine/service/INotificationService.java
+++ New Folder/WAMP_blog_engine/src/main/java/cs544/wamp_blog_engine/service/INotificationService.java
@@ -5,6 +5,7 @@
  */
 package cs544.wamp_blog_engine.service;
 
+import cs544.wamp_blog_engine.domain.Comment;
 import cs544.wamp_blog_engine.domain.Post;
 import cs544.wamp_blog_engine.domain.User;
 import java.util.List;
@@ -17,8 +18,9 @@ public interface INotificationService {
 
     public void notifyFollowers(List<User> followers, Post post);
 
-    public void notifyBlogger(User user);
+    public void notifyBlogger(List<User> user,String message);
+    public void notifyBloggerNewComment(List<User> user,Comment comment);
 
-    public void contactAdmin();
+    public void contactAdmin(User user,String message);
 
 }
