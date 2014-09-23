17391c9ef2d7755270d09361ac26cb630a3210e0
Weldu Berhe <wel.berhe@gmail.com>
2014-09-23 17:51:31 -0500

    Merge origin/master
    Conflicts:
    	New Folder/WAMP_blog_engine/src/main/java/cs544/wamp_blog_engine/service/impl/NotificationService.java

--- New Folder/WAMP_blog_engine/src/main/java/cs544/wamp_blog_engine/service/impl/NotificationService.java
+++ New Folder/WAMP_blog_engine/src/main/java/cs544/wamp_blog_engine/service/impl/NotificationService.java
@@ -92,8 +92,8 @@ public class NotificationService implements INotificationService {
     public void notifyBloggerNewComment(User user, Comment comment) {
         SimpleMailMessage template = getToBloggerTemplate();
         System.out.println( user.getFirstname()+" "+user.getLastname()+":"+comment.getParentPost().getTitle()+":"+comment.getParentPost().getParentBlog().getName()+":"+comment.getComm_time());
-        String emailMessage=String.format(template.getText(), user.getFirstname()+" "+user.getLastname(),comment.getParentPost().getTitle(),comment.getParentPost().getParentBlog().getName(),comment.getComm_time());
-        sendMail(template.getFrom(), user.getEmail(), template.getSubject(), emailMessage);
+            String emailMessage=String.format(template.getText(), user.getFirstname()+" "+user.getLastname(),comment.getParentPost().getTitle(),comment.getParentPost().getParentBlog().getName(),comment.getComm_time());
+            sendMail(template.getFrom(), user.getEmail(), template.getSubject(), emailMessage);
     }
 
     @Override
