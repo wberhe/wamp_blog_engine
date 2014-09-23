f4df5aac28c6f817f009e5aa0d7840f3c9f89cc5
MShikder <MShikder@CS_LAB218-107.cs.mum.edu>
2014-09-23 17:54:34 -0500

    Merge origin/master

--- New Folder/WAMP_blog_engine/src/main/java/cs544/wamp_blog_engine/domain/Blogs.java
+++ New Folder/WAMP_blog_engine/src/main/java/cs544/wamp_blog_engine/domain/Blogs.java
@@ -8,7 +8,11 @@ package cs544.wamp_blog_engine.domain;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
 
 /**
  *
@@ -47,6 +51,7 @@ public class Blogs {
      * 
      * 
      */
+    @XmlElement(name="blog")
     public Collection<Blog> getBlog() {
         if (blog == null) {
             blog = new ArrayList<Blog>();
